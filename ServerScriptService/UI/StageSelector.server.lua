185fac77a796f33bba3e40445deafa365e5eaae1
inaara-101 <inaarabhaidani@gmail.com>
2026-02-22 12:16:19 -0500

    Create StageSelector.server.lua

---
+++ ServerScriptService/UI/StageSelector.server.lua
@@ -0,0 +1,46 @@
+local dataManager = require(game.ServerScriptService:WaitForChild("Core"):WaitForChild("DataManager"))
+local remoteEvents = game.ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("Events")
+local configurations = game.ReplicatedStorage:WaitForChild("Configurations")
+
+game.Players.PlayerAdded:Connect(function(player)
+		
+	while not dataManager.Profiles[player] do
+		task.wait()
+	end
+	
+	local profile = dataManager.Profiles[player]
+	
+	if not profile then
+		return
+	end
+	
+	remoteEvents.SendStageData:FireClient(player, profile.Data.Stage)
+	
+end)
+
+remoteEvents.StageSelectorRequest.OnServerEvent:Connect(function(player, target)
+	
+	local profile = dataManager.Profiles[player]
+	
+	if not profile then
+		return
+	end
+	
+	local maxStage = profile.Data.Stage
+	
+	if target < 1 or target > maxStage then
+		return
+	end
+	
+	player.leaderstats.Stage.Value = target
+	player.CurrentBiome.Value = workspace.Checkpoints:FindFirstChild(tostring(target)):GetAttribute("Biome")
+	
+	if player.Character then
+		if player.Character:FindFirstChild("HumanoidRootPart") then
+			player.Character.HumanoidRootPart.CFrame = workspace.Checkpoints:FindFirstChild(tostring(target)).Checkpoint.CFrame + Vector3.new(0, 2, 0)
+		end
+	end
+	
+	remoteEvents.StageSelectorConfirm:FireClient(player, target)
+		
+end)
