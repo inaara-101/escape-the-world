19434423e2f24195af52cb7f674de987840ed233
inaara-101 <inaarabhaidani@gmail.com>
2026-02-22 12:23:14 -0500

    Create Tracks.lua

---
+++ ReplicatedStorage/Music/Tracks.lua
@@ -0,0 +1,141 @@
+return {
+	
+	Grassland = {
+		{
+			SoundId = "rbxassetid://9047837587",
+			Name = "Children Parade",
+			Artist = "Kaptain",
+		},
+		{
+			SoundId = "rbxassetid://1837587107",
+			Name = "Galopade",
+			Artist = "Coloursound CD Series",
+		},
+		{
+			SoundId = "rbxassetid://1837587301",
+			Name = "Puppy Dog",
+			Artist = "Coloursound CD Series",
+		},
+		{
+			SoundId = "rbxassetid://1837587216",
+			Name = "Mini Polka",
+			Artist = "Coloursound CD Series"
+		},
+	},
+	
+	Cave = {
+		{
+			SoundId = "rbxassetid://1847686830",
+			Name = "Glow Worm Cave",
+			Artist = "Sonoton Vanguard",
+		},
+		{
+			SoundId = "rbxassetid://120987592577395",
+			Name = "Ancient War Flowers",
+			Artist = "High Dream & Low Capital",
+		},
+		{
+			SoundId = "rbxassetid://1836730908",
+			Name = "Uneasy Peace",
+			Artist = "Bruton",
+		},
+		{
+			SoundId = "rbxassetid://9042294698",
+			Name = "Never Gone Forever",
+			Artist = "Inside Tracks",
+		},
+	},
+	
+	Desert = {
+		{
+			SoundId = "rbxassetid://1845490105",
+			Name = "Fari",
+			Artist = "Afro Musique",
+		},
+		{
+			SoundId = "rbxassetid://1847818499",
+			Name = "Egyptian Gold",
+			Artist = "Sonoton Vanguard",
+		},
+		{
+			SoundId = "rbxassetid://1837932456",
+			Name = "Egyptian Toomb Underscore",
+			Artist = "Invasion From Mars",
+		},
+		{
+			SoundId = "rbxassetid://73896472523148",
+			Name = "Wonders of the Oasis",
+			Artist = "Poygus",
+		},
+	},
+	
+	Arctic = {
+		{
+			SoundId = "rbxassetid://98443919757089",
+			Name = "Obby Mudah Tapi Aku Jatoh",
+			Artist = "Arya Arsakha",
+		},
+		{
+			SoundId = "rbxassetid://1839806358",
+			Name = "Magic Land",
+			Artist = "Kosinus",
+		},
+		{
+			SoundId = "rbxassetid://1839805972",
+			Name = "Enchanting Choir",
+			Artist = "Kosinus",
+		},
+		{
+			SoundId = "rbxassetid://1839806561",
+			Name = "Potion and Spell",
+			Artist = "Kosinus",
+		},
+	},
+	
+	Underworld = {
+		{
+			SoundId = "rbxassetid://1837825257",
+			Name = "Tech War",
+			Artist = "Epic Score",
+		},
+		{
+			SoundId = "rbxassetid://1847020488",
+			Name = "Awaken",
+			Artist = "Sonoton Vanguard",
+		},
+		{
+			SoundId = "rbxassetid://1837845027",
+			Name = "Underworld",
+			Artist = "Epic Score",
+		},
+		{
+			SoundId = "rbxassetid://1848198787",
+			Name = "Final Termination",
+			Artist = "Sonoton Vanguard",
+		},
+	},
+	
+	Ocean = {
+		{
+			SoundId = "rbxassetid://9043828544",
+			Name = "Maui Beach",
+			Artist = "Selected Sound New Vinyl Series",
+		},
+		{
+			SoundId = "rbxassetid://9047134387",
+			Name = "Tropical Breeze",
+			Artist = "Sonoton Vanguard",
+		},
+		{
+			SoundId = "rbxassetid://9043828414",
+			Name = "Hawaiian Train",
+			Artist = "Selected Sound New Vinyl Series",
+		},
+		{
+			SoundId = "rbxassetid://9043828399",
+			Name = "Aloha Oe",
+			Artist = "Selected Sound New Vinyl Series",
+		},
+	},
+	
+}
